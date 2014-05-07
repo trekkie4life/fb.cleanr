e436e5dee551d09675a3ebd004c9a8783170794c
trekkie4life <armandoreed@gmail.com>
2014-05-06 21:29:29 -0700

    Add event.js & icon19.png

---
+++ event.js
@@ -0,0 +1,11 @@
+
+
+function clearIt() {
+	var elements = ['rightCol', 'groupsNav', 'appsNav', 'interestsNav', 'pagesNav'];
+
+	for(i = 0; i < elements.length; i++) {
+	   document.getElementById(elements[i]).style.display = "none";
+	}
+}
+
+clearIt();
