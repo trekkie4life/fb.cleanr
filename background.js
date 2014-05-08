61b110e19932c18f11d8320243b2edfff39d7f4a
trekkie4life <armandoreed@gmail.com>
2014-05-08 15:54:06 -0700

    Add background.js - incorporate Chrome API tools for adding listeners for active tabs

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-function fbmain(tab_url) {
+function fbMain(tab_url) {
 	var notice = false;
 
 	if ((tab_url.match("https://www.facebook.com/")) || (tab_url.match("http://www.facebook.com/"))) {
@@ -8,8 +8,9 @@ function fbmain(tab_url) {
 	return notice;
 }
 
+
 function loadEvent(tab) {
-	if (fbmain(tab.url)) {
+	if (fbMain(tab.url)) {
 		chrome.tabs.executeScript(tab.tabID, {file: "event.js"}, function(result){});
 	}
 }
@@ -17,22 +18,21 @@ function loadEvent(tab) {
 
 // tab listeners to know when to fire the event script
 
-
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-  chrome.tabs.get(activeInfo.tabId, function(tabObj) {
-    loadEvent(tabObj);
+  chrome.tabs.get(activeInfo.tabId, function(tab) {
+    loadEvent(tab);
   });
 });
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (fbmain(tab.url)) {
+  if (fbMain(tab.url)) {
     chrome.tabs.executeScript(tabId, {file: "event.js"}, function(result) {});
   }
 });
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (fbmain(request.tabs_host) === true) {
+    if (fbMain(request.tabs_host) === true) {
       sendResponse(true);
     } else {
       icon_path = request.default_icon_path;
